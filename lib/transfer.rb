3bee3df8b0ae6dde7db7c66721c52fc6b98d25ec
daniel campo <campo_daniel94@yahoo.com>
2020-08-27 22:42:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
     
     def execute_transaction
       binding.pry
-      if @sender.balance > @amount && @status == "pending"
+      if @sender.balance > @amount && @status == "pending"&& valid?
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"
